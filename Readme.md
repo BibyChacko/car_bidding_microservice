920ff21045f4fecb8e895c3f845adadc97af3cf9
almubdie <bibychackokpply@gmail.com>
2023-10-19 19:57:27 +0530

    Infra for all service updated

--- Readme.md
+++ Readme.md
@@ -11,3 +11,5 @@ npm i ../common_modules dotenv express mongoose chai chai-http mocha --save
 For intsalling ingress-nginx , use the following command
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/cloud/deploy.yaml
+
+#Access rabbitmq through http://localktzdev.com:15672/
--- common_modules/src/util/message_broker_util.js
+++ common_modules/src/util/message_broker_util.js
@@ -1,7 +1,7 @@
 const amqp = require("amqplib");
 
 const channel = async (queueName) =>{
-    const connection = await amqp.connect("amqp://localhost");
+    const connection = await amqp.connect("amqp://rabbit-mq-service");
     const channel =  await connection.createChannel();
     await channel.assertQueue(queueName,{durable:false});
     return channel;
